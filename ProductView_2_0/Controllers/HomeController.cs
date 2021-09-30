21b11189ace2ad18377bfa8f0c38d980cddeacf6
Никита Куваев <likeaboss13371@gmail.com>
2021-09-30 19:16:26 +0300

    Home Controller Correct

--- ProductView_2_0/Controllers/HomeController.cs
+++ ProductView_2_0/Controllers/HomeController.cs
@@ -20,12 +20,60 @@ namespace ProductView_2_0.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Products = ProductRep.GetProducts();
             return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult NewProduct() => View();
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Edit_product = ProductRep.GetProducts().Find(i => i.id == id);
+            return View();
+        }
+        public IActionResult Del(int id)
+        {
+            ProductRep.Delete(id);
+            return Redirect("/Home/Index");
+        }
+        public IActionResult EditProduct(Product product)
+        {
+            if (CheckProduct(product))
+                ProductRep.UpdateProducts(product);
+            return Redirect("/Home/Index");
+        }
+        public IActionResult Add(Product product)
+        {
+            if (CheckProduct(product))
+                ProductRep.AddProducts(product);
+            
+            return Redirect("/Home/NewProduct");
+        }
+        private bool CheckProduct(Product product)
+        {
+            bool rez = false;
+            if (product.model != null && product.model != string.Empty)
+                if (product.product != null && product.product != string.Empty)
+                    if (product.salesman != null && product.salesman != string.Empty)
+                        if (product.category != null && product.category != string.Empty)
+                            if (product.description != null && product.description != string.Empty)
+                                if (product.price != null)
+                                    if (product.img != null && product.img != string.Empty && IsUrl(product.img))
+                                        rez = true;
+            return rez;
+        }
+        private bool IsUrl(string url)
+        {
+            bool rez;
+            if (Uri.IsWellFormedUriString(url, UriKind.Absolute) && url.EndsWith(".png") || url.EndsWith(".jpg"))
+                rez = true;
+            else
+                rez = false;
+            return rez;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

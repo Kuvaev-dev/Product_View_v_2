7e7b331317d3086079a0b1e3659ebaa383c14dd2
Никита Куваев <likeaboss13371@gmail.com>
2021-09-30 16:09:26 +0300

    Init

---
+++ ProductView_2_0/Models/Pages/Carousel.cshtml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Product_View_v_2.Views.Home
+{
+    public class CarouselModel : PageModel
+    {
+        public void OnGet() {}
+    }
+}

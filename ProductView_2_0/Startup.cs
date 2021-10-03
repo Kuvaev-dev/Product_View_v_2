370379c902e920542dd0514a89117b979dd45f03
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2021-10-03 11:57:38 +0300

    Update Startup.cs

--- ProductView_2_0/Startup.cs
+++ ProductView_2_0/Startup.cs
@@ -20,10 +20,11 @@ namespace ProductView_2_0
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+       // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +49,7 @@ namespace ProductView_2_0
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
